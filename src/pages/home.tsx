880cfd8659316a02d1ba7c157e5a8e6f22c55216
Bernardo <bernardoorosa@gmail.com>
2021-08-19 20:58:13 -0300

    Fetch data from API

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import useSWR from "swr";
 
+type GithubResponse = {
+    incomplete_results: boolean,
+    items: any[],
+    total_count: number
+}
 const Home = () => {
+    const fetcher = (input: string ) => fetch("https://api.github.com/search/users?q="+input).then(res => res.json());
+    
+    const [searchInput, setSearchInput] = useState("");
+    const inputEl = useRef<HTMLInputElement>(null);
+    const { data, error } = useSWR<GithubResponse, any>(searchInput ?
+        searchInput : null,
+        fetcher
+    );
+    useEffect(() => {
+        if(data){
+            console.log(data)
+        }
+    }, [data]);
     return (
+        <main>
         <header>Github Users</header>
+        <section>
+            <input placeholder="user-search" name="input"
+                ref={inputEl}
+            />
+            <button onClick={() => setSearchInput(inputEl?.current?.value || "")} type="submit">Search</button>
+            {data && data.items.map(item => {
+                return <span>{item.login}</span>
+            })}
+        </section>
+        </main>
+        
     )
 }
 
