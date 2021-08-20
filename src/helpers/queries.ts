114f527802bb0b518d32ab52a4e61d495fbd9786
Bernardo <bernardoorosa@gmail.com>
2021-08-20 15:37:16 -0300

    Create graphql queries

---
+++ src/helpers/queries.ts
@@ -0,0 +1,57 @@
+import { gql } from "@apollo/client"
+
+
+export interface UserNode {
+    node: {
+        avaterUrl: string;
+        bio: string | null;
+        followers: {
+            totalCount: number;
+        }
+        following: {
+            totalCount: number;
+        }
+        starredRepositories: {
+            totalCount: number;
+        }
+        login: string;
+        name: string;
+    }
+}
+
+export interface GithubUsersSearch {
+    search: {
+        edges: UserNode[];
+        userCount: number;
+    }
+}
+
+export interface SearchUsersVars {
+    first: number;
+    query: string;
+}
+export const SEARCH_USERS = gql`
+  query Search($query: String!, $first: Int!){
+    search(query: $query, type: USER, first: $first){
+        userCount
+        edges {
+            node {
+                ... on User {
+                    followers{
+                        totalCount
+                    }
+                    following {
+                        totalCount
+                    }
+                    starredRepositories {
+                        totalCount
+                    }
+                    avatarUrl
+                    bio
+                    name
+                    login
+                }
+            }
+        }
+    }
+}`;
