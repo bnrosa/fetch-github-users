add7dcd9f017a1f74372dc41aee3e0d39e11cf46
Bernardo <bernardoorosa@gmail.com>
2021-08-20 09:06:46 -0300

    Add Chakra UI components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import useSWR from "swr";
+import {Flex, Heading, Input, Button, Select, Container, Text} from "@chakra-ui/react"
 
 type GithubResponse = {
     incomplete_results: boolean,
@@ -7,12 +8,13 @@ type GithubResponse = {
     total_count: number
 }
 const Home = () => {
-    const fetcher = (input: string ) => fetch("https://api.github.com/search/users?q="+input).then(res => res.json());
+    const fetcher = (query: string ) => fetch(`https://api.github.com/search/users${query}`).then(res => res.json());
     
     const [searchInput, setSearchInput] = useState("");
+    const [searchOrder, setSearchOrder] = useState("desc");
     const inputEl = useRef<HTMLInputElement>(null);
-    const { data, error } = useSWR<GithubResponse, any>(searchInput ?
-        searchInput : null,
+    const { data, error, mutate } = useSWR<GithubResponse, any>(searchInput ?
+        `?q=${searchInput}&order=${searchOrder}` : null,
         fetcher
     );
     useEffect(() => {
@@ -21,18 +23,30 @@ const Home = () => {
         }
     }, [data]);
     return (
-        <main>
-        <header>Github Users</header>
-        <section>
-            <input placeholder="user-search" name="input"
-                ref={inputEl}
-            />
-            <button onClick={() => setSearchInput(inputEl?.current?.value || "")} type="submit">Search</button>
-            {data && data.items.map(item => {
-                return <span>{item.login}</span>
-            })}
-        </section>
-        </main>
+        <Flex height="100vh" justifyContent="center" background="gray.100" p={16}>
+            <Container direction="column" background="white" p={8} rounded={6}>
+                <Heading mb={6}>Github Users</Heading>
+                <Flex direction="row" mb={2} >
+                    <Flex direction="column" p={2}>
+                        <Text>User name:</Text>
+                        <Input placeholder="user-search" name="input"
+                        ref={inputEl} type="text" mr={2}
+                    />
+                    </Flex>   
+                    <Flex direction="column" p={2}>
+                        <Text>Order: </Text>
+                        <Select onChange={(e) => {setSearchOrder(e.target.value); mutate();}}>
+                            <option value="desc">Descending</option>
+                            <option value="asc">Ascending</option>
+                        </Select>
+                    </Flex>   
+                </Flex>
+                <Button onClick={() => {setSearchInput(inputEl?.current?.value || ""); mutate(); }} type="submit">Search</Button>
+                    {data?.items && data.items.map(item => {
+                        return <Text p={4} mb={2}>{item.login}</Text>
+                    })}
+            </Container>
+        </Flex>
         
     )
 }
