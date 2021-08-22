5549dab396189663790b81c83887a27ca217b8d6
Bernardo <bernardoorosa@gmail.com>
2021-08-22 16:47:01 -0300

    Improve legibility with deconstructuring on UserTableRow

--- src/components/UserTableRow.tsx
+++ src/components/UserTableRow.tsx
@@ -6,19 +6,20 @@ interface UserTableRowProps {
   user: UserNode;
 }
 
-const UserTableRow: FC<UserTableRowProps> = ({user}) => {
+const UserTableRow: FC<UserTableRowProps> = (props) => {
+    const {url, avatarUrl, followers, following, name, login, bio, starredRepositories} = props.user.node;
     return( 
-    <a href={user.node.url} target="_blank" rel="noreferrer">
+    <a href={url} target="_blank" rel="noreferrer">
       <WrapItem my={2} p={2} bg="gray.50" boxShadow="md" rounded="md" alignItems="center">
-        <Avatar size="xl" name={user.node.login} src={user.node.avatarUrl} />
+        <Avatar size="xl" name={login} src={avatarUrl} />
         <Flex direction="column" ml={2} px={2} py={1}>
-          <Text textAlign="center" my={1} fontSize="lg" fontWeight="bold">{user.node.name || "no_name"} - {user.node.login || "no_username"}</Text>
+          <Text textAlign="center" my={1} fontSize="lg" fontWeight="bold">{name || "no_name"} - {login || "no_username"}</Text>
           <Text color="gray.700" fontSize="xs" my={1}>
-            <strong>Followers: </strong> {user.node.followers?.totalCount || 0}{"\t"}
-            <strong>Following: </strong> {user.node.following?.totalCount || 0}{"\t"}
-            <strong>Starred Repos: </strong> {user.node.starredRepositories?.totalCount || 0}
+            <strong>Followers: </strong> {followers?.totalCount || 0}{"\t"}
+            <strong>Following: </strong> {following?.totalCount || 0}{"\t"}
+            <strong>Starred Repos: </strong> {starredRepositories?.totalCount || 0}
           </Text>
-          <Text color="gray.600" my={1} fontSize="sm">{user.node.bio || "I have no bio."}</Text>
+          <Text color="gray.600" my={1} fontSize="sm">{bio || "I have no bio."}</Text>
         </Flex>
       </WrapItem>
     </a>
